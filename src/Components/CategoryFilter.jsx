06d9a2c6ea7f071619ea676e220a2434ebb1d381
Muhammad Huzaifa Ghazali <huzaifaghazali@gmail.com>
2023-06-16 21:48:45 +0500

    Render all the categories

--- src/Components/CategoryFilter.jsx
+++ src/Components/CategoryFilter.jsx
@@ -1,9 +1,46 @@
-import React from 'react'
+import React from 'react';
+import styled from 'styled-components';
+
+const CATEGORIES = [
+  { name: 'technology', color: '#3b82f6' },
+  { name: 'science', color: '#16a34a' },
+  { name: 'finance', color: '#ef4444' },
+  { name: 'society', color: '#eab308' },
+  { name: 'entertainment', color: '#db2777' },
+  { name: 'health', color: '#14b8a6' },
+  { name: 'history', color: '#f97316' },
+  { name: 'news', color: '#8b5cf6' },
+];
 
 const CategoryFilter = () => {
   return (
-    <div>CategoryFilter</div>
-  )
-}
+    <Wrapper>
+      <ul>
+        <li className='category'>
+          <button className='btn btn-all-categories'>All</button>
+        </li>
 
-export default CategoryFilter
+        {CATEGORIES.map((category) => {
+          return (
+            <li key={category.name} className='category'>
+              <button
+                className='btn btn-category'
+                style={{ backgroundColor: category.color }}
+              >
+                {category.name}
+              </button>
+            </li>
+          );
+        })}
+      </ul>
+    </Wrapper>
+  );
+};
+
+export default CategoryFilter;
+
+const Wrapper = styled.aside`
+  .category {
+    margin-bottom: 1.6rem;
+  }
+`;
